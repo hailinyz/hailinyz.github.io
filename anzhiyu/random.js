779f6a92ce1f885504db98db1aa485984dba9123
hailinyz <yuanzhangliang3@gmail.com>
2025-08-04 12:02:54 +0800

    Site updated: 2025-08-04 12:02:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/这是另一篇新的博文/","undefined/这是一篇新的博文/","undefined/hello-world/"];function toRandomPost(){
+var posts=["wenzhang/这是另一篇新的博文/","wenzhang/这是一篇新的博文/","wenzhang/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
