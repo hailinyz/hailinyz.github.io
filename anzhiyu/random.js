da0eda7e7c2324a0431e2fbb0b8792781a6e69f8
hailinyz <yuanzhangliang3@gmail.com>
2025-08-14 18:12:04 +0800

    Site updated: 2025-08-14 18:12:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/08/第一期：HailinMusicPlayer 安装使用指南改/"];function toRandomPost(){
+var posts=["2025/08/08/第一期：关于如何下载 HailinMusicPlayer 以及安装使用指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
