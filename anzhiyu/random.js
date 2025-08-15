deb28b420de5f1503509f15ffb3ec214082b3781
hailinyz <yuanzhangliang3@gmail.com>
2025-08-15 18:37:20 +0800

    Site updated: 2025-08-15 18:37:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/08/第一期：关于如何下载 HailinMusicPlayer 以及安装使用指南/","2025/08/08/第三期：海林影视影视聚合网站使用指南/","2025/08/08/第四期：HailinTV-网站进阶指南/","2025/08/08/第二期：HailinMusicPlayer 使用指南/"];function toRandomPost(){
+var posts=["2025/08/08/第一期：关于如何下载 HailinMusicPlayer 以及安装使用指南/","2025/08/08/第三期：海林影视影视聚合网站使用指南/","2025/08/08/第二期：HailinMusicPlayer 使用指南/","2025/08/08/第四期：HailinTV-网站进阶指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
